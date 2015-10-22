f17cd7e1c112674abc0cbb3d797331624d21a703
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2015-10-22 05:12:36 +0100

    Woah it works O.o

--- src/Board.java
+++ src/Board.java
@@ -1,39 +1,106 @@
+
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
 
 /**
  * Created by Aaron on 21/10/2015.
  */
 public class Board extends JPanel implements ActionListener{
     private static final int DELAY = 25;
-    private static boolean showMenu = true;
     private static int mouseMotion = 5000;
     private Timer timer;
+    private boolean started = false;
+    private long startTime;
+    private long timeRemaining;
+    private long spentTime;
 
 
+    private ArrayList<JButton> menu;
 
+    private JLabel countDown;
+    private JButton pause;
+    private JButton start;
+
+    private boolean startVisible = true;
 
 
     public Board() {
         addMouseMotionListener(new MAdapter());
-
+        setLayout(new GridBagLayout());
         setBackground(Color.black);
         setFocusable(true);
         setDoubleBuffered(true);
 
+        spentTime = 0;
+
+        menu = Main.menu;
+
+        initComponents();
         initApp();
 
     }
 
+    private void initComponents() {
+        GridBagConstraints c = new GridBagConstraints();
+        c.anchor = GridBagConstraints.SOUTH;
+
+        c.weighty = 10;
+        c.gridy = 2;
+
+        countDown = new JLabel();
+        countDown.setFont(new Font(null, Font.PLAIN, 200));
+        countDown.setForeground(Color.white);
+        add(countDown, c);
+
+        c = new GridBagConstraints();
+
+        pause = new JButton("Pause");
+        pause.addActionListener(new PauseListener());
+        pause.setFont(new Font(null, Font.PLAIN, 48));
+        c.anchor = GridBagConstraints.CENTER;
+
+        c.gridy = 1;
+        add(pause, c);
+
+        c = new GridBagConstraints();
+
+        start = new JButton("Start");
+        start.addActionListener(new StartListener());
+        start.setFont(new Font(null, Font.PLAIN, 48));
+        c.anchor = GridBagConstraints.CENTER;
+
+        c.gridy = 1;
+
+
+
+        add(start, c);
+
+
+    }
+
     public void initApp(){
+
+        startTime = System.currentTimeMillis();
+
         timer = new Timer(DELAY, this);
         timer.start();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
+
         mouseMotion -= DELAY;
+        if(mouseMotion < 0)menuVisible(false);
+
+        if(started) {
+            timeRemaining = startTime + (Main.drinkingTime - Main.startTime) * 60000 - System.currentTimeMillis() - spentTime;
+            countDown.setText(processClock(timeRemaining));
+        }else {
+            countDown.setText(processClock((Main.drinkingTime - Main.startTime)* 60000 - spentTime));
+        }
         repaint();
     }
 
@@ -46,6 +113,7 @@ public class Board extends JPanel implements ActionListener{
     private void doDrawing(Graphics g) {
 
 
+
         Toolkit.getDefaultToolkit().sync();
     }
 
@@ -55,8 +123,83 @@ public class Board extends JPanel implements ActionListener{
         @Override
         public void mouseMoved(MouseEvent e){
             mouseMotion = 5000;
+            menuVisible(true);
 
         }
 
+    }
+
+    private void menuVisible(boolean b){
+        for(JButton button : menu){
+            button.setVisible(b);
+        }
+        if(b){
+            start.setVisible(startVisible);
+            pause.setVisible(!startVisible);
+        }else{
+            start.setVisible(false);
+            pause.setVisible(false);
+        }
+
+    }
+
+    private String processClock(long time){
+            int minutes = 0;
+            int seconds = 0;
+            int milliseconds;
+
+            String mins;
+            String secs;
+            String mills;
+
+            while (time >= 60000){
+                minutes+=1;
+                time -= 60000;
+            }
+            if(minutes < 10){
+                mins = "00" + minutes;
+            }else if(minutes < 100){
+                mins = "0" + minutes;
+            }
+            else{
+                mins = "" + minutes;
+            }
+
+            while (time >= 1000) {
+                seconds += 1;
+                time -= 1000;
+            }
+            if(seconds < 10){
+                secs = "0"+seconds;
+            }else {
+                secs = "" + seconds;
+            }
+            milliseconds = (int) time;
+            if(milliseconds < 10){
+                mills = "00" + milliseconds;
+            }else if(milliseconds < 100){
+                mills = "0" + milliseconds;
+            }
+            else{
+                mills = "" + milliseconds;
+            }
+        return mins+":"+secs+":"+mills;
+    }
+
+    private class StartListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            startTime = System.currentTimeMillis();
+            startVisible = false;
+            started = true;
+        }
+    }
+    private class PauseListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            spentTime = (Main.drinkingTime - Main.startTime)* 60000 - timeRemaining;
+            startVisible = true;
+            started = false;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,29 @@
 import javax.swing.*;
+import javax.swing.text.JTextComponent;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 /**
  * Created by Aaron on 21/10/2015.
  */
 public class Main extends JFrame{
-    private JButton fullscreenButton;
+    private JButton fullscreen;
+    private JButton close;
+    private JButton settings;
+    public static JPanel menuBox = new JPanel();
+
+    public static ArrayList<JButton> menu;
+    JTextField timeBox;
+    JTextField startBox;
+
+    private JFrame options;
+    private ArrayList<JPanel> rows;
+
+    public static int drinkingTime;
+    public static int startTime;
+
     private boolean Am_I_In_FullScreen;
     private int PrevX,PrevY,PrevWidth,PrevHeight;
     Board board;
@@ -25,24 +41,42 @@ public class Main extends JFrame{
     }
 
     public Main() {
+        menu = new ArrayList<JButton>();
         board = new Board();
         Am_I_In_FullScreen = false;
         setContentPane(board);
         pack();
 
-        fullscreenButton = new JButton("Fullscreen");
+        settings = new JButton("Settings");
+        menu.add(settings);
+        settings.addActionListener(new SettingsListener());
 
-
-
+        fullscreen = new JButton("Fullscreen");
+        menu.add(fullscreen);
         FullScreenEffect effect = new FullScreenEffect();
-        fullscreenButton.addActionListener(effect);
-        //fullscreenButton.setLocation(100, 100);
+        fullscreen.addActionListener(effect);
+
+        close = new JButton("Close");
+        menu.add(close);
+        close.addActionListener(new CloseListener());
+
+
+        JPanel menuBox = makeRow(settings, fullscreen, close);
+        GridBagConstraints c = new GridBagConstraints();
+        c.anchor = GridBagConstraints.NORTH;
+        c.gridy = 0;
+        board.add(menuBox,c);
+
+        settings.setVisible(true);
+        fullscreen.setVisible(true);
+        close.setVisible(true);
 
 
 
-        board.add(fullscreenButton);
-        fullscreenButton.setVisible(true);
+        rows = new ArrayList<JPanel>();
 
+        drinkingTime = 100;
+        startTime = 0;
 
     }
 
@@ -79,4 +113,88 @@ public class Main extends JFrame{
             }
         }
     }
+    private class CloseListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.exit(0);
+        }
+    }
+
+    private class SettingsListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            rows = new ArrayList<JPanel>();
+            options = new JFrame();
+
+            JLabel time = new JLabel("Centurion Length:");
+            time.setForeground(Color.white);
+            timeBox = new JTextField("" + drinkingTime,3);
+            rows.add(makeRow(time,timeBox));
+
+            JLabel start = new JLabel("Start Time:");
+            start.setForeground(Color.white);
+            startBox = new JTextField("" + startTime,3);
+            rows.add(makeRow(start,startBox));
+
+
+
+
+            options.setLayout(new GridLayout(rows.size() + 1, 0));
+
+            for(JPanel row : rows){
+                options.add(row);
+            }
+
+            JButton okay = new JButton("Okay");
+            okay.addActionListener(new OkayListener());
+            JButton cancel = new JButton("Cancel");
+            cancel.addActionListener(new CancelListener());
+            options.add(makeRow(okay,cancel));
+
+            options.setSize(300, 200);
+            options.pack();
+            options.setResizable(false);
+            options.setVisible(true);
+
+
+        }
+    }
+
+    private static JPanel makeRow(Component... components){
+        JPanel row = new JPanel();
+        row.setLayout(new FlowLayout());
+        row.setBackground(Color.black);
+        for(Component component : components){
+            row.add(component);
+        }
+        return row;
+    }
+
+    private class OkayListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                startTime = Integer.parseInt(startBox.getText());
+                drinkingTime = Integer.parseInt(timeBox.getText());
+                options.setVisible(false);
+            }catch (NumberFormatException exception){
+                JOptionPane.showMessageDialog(null,"Numbers Plz");
+            }
+        }
+    }
+
+    private class CancelListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            options.setVisible(false);
+        }
+    }
+
+    private class ResetListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
 }
