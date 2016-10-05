29ef09ecbaa3d2ee8f66cf5156a7e42fcd8a1133
chalamander <charliepostgate@hotmail.co.uk>
2016-10-05 03:41:13 +0100

    improved "Drink"! message & background. Code optimisations

--- src/Board.java
+++ src/Board.java
@@ -1,13 +1,9 @@
 
 
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
 
 /**
