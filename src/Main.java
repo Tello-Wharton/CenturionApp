32a903e7f4933a42f0b4288485adc6bae72f51c9
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2017-10-17 13:26:04 +0100

    cleaned up main

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
-import javax.swing.*;
 import java.awt.*;
 
 public class Main {
 
     public static void main(String[] args){
-        
+
         EventQueue.invokeLater(() -> {
 
                 Window ex = new Window();
